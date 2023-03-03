5b068069f0f0c4762b3f679063fa7fae2d361e58
Alper ARI <alper.hamza.ari@gmail.com>
2023-03-03 18:52:29 +0300

    Initial commit

---
+++ test/aws-cdk-staticweb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkStaticweb from '../lib/aws-cdk-staticweb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-staticweb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkStaticweb.AwsCdkStaticwebStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
