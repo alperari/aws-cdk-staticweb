58d234a8a6ee4ebe67c7ecc6d87193b950ebcc64
Alper ARI <alper.hamza.ari@gmail.com>
2023-03-03 19:50:14 +0300

    Create bucket and upload static content

--- lib/stacks/aws-cdk-staticweb-stack.ts
+++ lib/stacks/aws-cdk-staticweb-stack.ts
@@ -7,5 +7,25 @@ import { Construct } from "constructs";
 export class AwsCdkStaticwebStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create bucket named 'static-website-bucket'
+    // Make it publicly readable so that when website is hosted, users can open it on their browsers
+    // Specify index and error files (Specifying explicitly will enable hosting)
+    const bucket = new s3.Bucket(this, "static-website-bucket", {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+    });
+
+    // Upload static content into the bucket
+    const deployment = new s3deploy.BucketDeployment(
+      this,
+      "DeployStaticWebsite",
+      {
+        sources: [s3deploy.Source.asset("../src/staticweb")],
+        destinationBucket: bucket,
+      }
+    );
   }
 }
